86cb3a8782a5c3007fe251b4606016c91cdfc1b9
Elizer Dela Cruz <elidc93two@yahoo.com>
2023-01-23 04:03:30 +0800

    UI improvements, added anime.min.js

--- main.js
+++ main.js
@@ -12,19 +12,21 @@ var hour = h1.innerHTML + h2.innerHTML;
 var minute = m1.innerHTML + m2.innerHTML;
 var second = s1.innerHTML + s2.innerHTML;
 
-
-console.log(hour)
-console.log(minute)
-console.log(second)
+// anime({
+//     targets: 'span',
+//     translateX: 250,
+//     rotate: '1turn',
+//     backgroundColor: '#FFF',
+//     duration: 800
+//   });
 
 setInterval(function () {
-
     if (parseInt(second) == 0) {
         if (parseInt(minute) == 0) {
             if (parseInt(hour) > 0) {
                 second = 59;
                 updateDigits('s', second)
-
+            
                 minute = 59;
                 updateDigits('m', minute)
 
@@ -32,21 +34,25 @@ setInterval(function () {
                 updateDigits('h', hour)
             }
         } else if (parseInt(minute) > 0) {
-            minute = minute - 1;
-            updateDigits('m', minute)
 
             second = 59;
             updateDigits('s', second)
+
+            minute = minute - 1;
+            updateDigits('m', minute)
         }
     } else if (parseInt(second) > 0) {
         second = second - 1;
         updateDigits('s', second)
     }
+    updateTitle(hour, minute, second)
 }, 1000);
 
 function updateDigits(mode, digit) {
     var element1;
     var element2;
+    var elementsToAnimate;
+
     switch(mode) {
         case 's':
             element1 = s1;
@@ -69,19 +75,40 @@ function updateDigits(mode, digit) {
         console.log()
         element1.innerHTML = digitString[0];
         element2.innerHTML = digitString[1];
+        elementsToAnimate = `#${element1.id}, #${element2.id}`
     } else {
         element1.innerHTML = 0;
         element2.innerHTML = digit;
+        elementsToAnimate = `#${element2.id}`
+        // animateDigits(elementsToAnimate);
+        // pause(target)
     }
+    // animateDigits(elementsToAnimate);
 }
 
-// minute = digit
-// if (minute > 9) {
-//     var minuteString = Array.from(String(minute), Number);
-//     console.log()
-//     m1.innerHTML = minuteString[0];
-//     m2.innerHTML = minuteString[1];
-// } else {
-//     m1.innerHTML = 0;
-//     m2.innerHTML = minute;
-// }
+function pause(target) {
+    console.log(target);
+    anime({
+        targets: target
+    }).pause();
+}
+
+function animateDigits(target) {
+    console.log(target);
+    anime({
+        targets: target,
+        // rotateX: '90',
+        easing: 'easeInOutQuad',
+        rotateX: '1turn',
+        duration: 800,
+        loop: true
+    }).play();
+}
+
+function updateTitle(hour, minute, second) {
+    var hourString = (hour == 0) ? "00" : String(hour);
+    var minuteString = (minute == 0) ? "00" : String(minute);
+    var secondString = (second == 0) ? "00" : String(second);
+
+    document.title = `${hourString}:${minuteString}:${secondString}`
+}
